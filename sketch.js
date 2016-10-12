5320a01b0a279cd4284a84cfc6f9d6050c3f1d70
lee sang min <leeg7200@naver.com>
2016-10-12 15:42:54 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800,600);
+}
+
+function draw() {
+  background(200,50,79); 
+ rect(200,100,300,300)
+ fill(10,200,153)
+ noStroke();
+ ellipse(100,100,150,150)
+}
